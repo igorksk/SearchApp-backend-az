6ebfaa31064541fdce80f6c5385069078e349d5f
Igor Kaseko <igorksk@gmail.com>
2025-07-05 20:31:15 +0300

    Hide Swagger

--- SearchApi/Program.cs
+++ SearchApi/Program.cs
@@ -50,14 +50,11 @@ namespace SearchApi
             }
 
             // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
-
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
             app.UseCors("MyCorsPolicy");
